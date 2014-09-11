ca4a3333a033de1d301e04391defff8b481a15f2
Adam Carter <adamrcarter@gmail.com>
2014-09-10 21:25:47 -0500

    attempt at rspec tests...

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Article, :type => :model do
-  
+  it "returns first 30 characters of the articles body" do
+    body_array.should eq(30)
+end
+it "returns short, medium, or long" do
+  if array_length >= 300
+    array_length.should eq("Long! (Want fries with that?)")
+end
 end
