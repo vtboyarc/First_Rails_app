1a3790e79b0502f64e41f9f1dc13097679d662a6
Adam Carter <adamrcarter@gmail.com>
2014-09-10 19:31:15 -0500

    added Tag model

---
+++ db/migrate/20140911003012_create_tags.rb
@@ -0,0 +1,10 @@
+class CreateTags < ActiveRecord::Migration
+  def change
+    create_table :tags do |t|
+      t.string :name
+      t.string :skill
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140911000130) do
+ActiveRecord::Schema.define(:version => 20140911003012) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(:version => 20140911000130) do
   create_table "links", :force => true do |t|
     t.string   "name"
     t.string   "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.string   "skill"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
