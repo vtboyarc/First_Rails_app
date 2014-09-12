31a2b5466daf11f200e65fb29ae3946914bef233
Adam Carter <adamrcarter@gmail.com>
2014-09-11 19:26:51 -0500

    continued work on user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  #if there is a value in session user_id, give back the user who's id is that session id
+  #this method is available in every controller
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    end
+  end
+  
+  #this allows the current_user method to be available in all the views
+  helper_method :current_user
 end
