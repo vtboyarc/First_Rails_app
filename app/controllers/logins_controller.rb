880cde01357bb8b7f2e0f539d349a888eec74562
Adam Carter <adamrcarter@gmail.com>
2014-09-24 20:25:28 -0500

    wip

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,13 +1,13 @@
 class LoginsController < ApplicationController
-   skip_before_filter :authorize, :only => [:new, :create]
+   skip_before_filter :authorize
   def new
   end
   
   def create
-    user = User.find_by_email(params[:email])
+    @user = User.find_by_email(params[:email])
     
-      if user && user.authenticate(params[:password]) #if user && is to keep from being nil
-        session[:user_id] = user.id
+      if @user && @user.authenticate(params[:password]) #if user && is to keep from being nil
+        session[:user_id] = @user.id
         redirect_to articles_path
       else
         raise "Invalid login."
